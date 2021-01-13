3aabce4dd6905201c6a641b3b90286df38cbc3ea
Jerome Pramondon <jpramondon@norauto.com>
2021-01-13 14:19:59 +0100

    Removed extraneous scripts

--- scripts/22_start_2_composed_apis.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-API_IMAGE_TAG="permitservice2:latest" docker-compose -f ./test/2_masters/docker-compose.yml up
--- scripts/22_start_composed_api_daemon.sh
+++ scripts/22_start_composed_api_daemon.sh
@@ -3,4 +3,4 @@
 # export PG_PORT=5432
 # export DOCKER_API_PORT=$API_PORT:
 # export DOCKER_PG_PORT=$PG_PORT:
-API_IMAGE_TAG="permitservice2:latest" APP_CONF_PATH="./conf" APP_ENV='local' docker-compose up -d
+API_IMAGE_TAG="gearedminds/permit:latest" APP_CONF_PATH="./conf" APP_ENV='local' docker-compose up -d
